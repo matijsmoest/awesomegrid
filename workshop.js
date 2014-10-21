2304066d81ca91af277a7a4bb50afc97afe62ed5
unknown <MMOEST@LBNL02231.corp.capgemini.com>
2014-10-21 12:32:20 +0200

    first awesome version

---
+++ workshop.js
@@ -0,0 +1,52 @@
+angular.module("workshop", [])
+
+	.filter("foo", function() {
+	
+	})
+	
+	.controller("AppCtrl", function AppCtrl($http) {
+		var app = this;
+		app.url = "http://localhost:3000/game";
+		
+		$http.get(app.url)
+			.then(function(response) {
+				console.log(response);
+				app.data = response.data;
+			});
+			
+		app.setOrder = function(order) {
+			if (app.order === order) {
+				app.order = "-"+order;
+			} else {
+				app.order = order;
+			}
+		};
+		
+		app.selectGame = function(id) {
+			
+			console.log(id);
+			
+			$http.get(app.url + "/" + id)
+				.then(function(result) {
+					app.selectedGame = result.data;
+				});
+		};
+		
+		app.updateSelectedGame = function(game) {
+			$http.put(app.url + "/" + app.selectedGame.gameId, game)
+				.then(function(result) {
+					app.selectGame(game.gameId);
+				});
+		};
+		
+		app.getGames = function() {
+			$http.get(app.url)
+				.then(function(response) {
+					console.log(response);
+					app.data = response.data;
+				});
+		};
+		
+		
+		
+	})
